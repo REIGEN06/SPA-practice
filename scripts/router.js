fdd40adac6867a2d167a9f9186290598654d33bc
REIGEN <daniil-yak@yandex.ru>
2023-04-25 21:15:16 +0300

    logs

--- scripts/router.js
+++ scripts/router.js
@@ -20,6 +20,9 @@ const handleLocation = async () => {
   const path = window.location.pathname;
   const route = routes[path] || routes[404];
   const html = await fetch(route).then((data) => data.text());
+  console.log(path);
+  console.log(route);
+  console.log(html);
   document.getElementById("app").innerHTML = html;
 };
 // Отслеживает изменение юрла (listener)
