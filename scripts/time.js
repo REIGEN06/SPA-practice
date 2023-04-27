8cfaf43a97fd3326775a1d576c9108e72af1fea5
REIGEN <daniil-yak@yandex.ru>
2023-04-27 15:10:16 +0300

    fix: newTimeFunction

--- scripts/time.js
+++ scripts/time.js
@@ -1,5 +1,4 @@
 let startTime = Date.parse(new Date());
-localStorage.setItem("startTime", startTime);
 
 let checker = setInterval(function () {
   let timer = document.getElementById("time");
@@ -9,15 +8,7 @@ let checker = setInterval(function () {
     function getTime() {
       let currentTime = Date.parse(new Date());
       let liveTime = currentTime - startTime;
-      console.log(liveTime);
-
-      return [
-        (liveTime % 86400000) / 3600000,
-        (liveTime % 3600000) / 60000,
-        (liveTime % 60000) / 1000,
-      ]
-        .map((value) => Math.round(value).toString().padStart(2, "0"))
-        .join(":");
+      return new Date(liveTime).toUTCString().substring(17, 25);
     }
   }
 }, 1000);
