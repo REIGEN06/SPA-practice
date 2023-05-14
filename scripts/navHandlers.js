5bde316bb82a73039d07c9dba13fc289cdd4fbaf
REIGEN <daniil-yak@yandex.ru>
2023-05-14 15:06:38 +0300

    fix: EventListener for window, hash to pathname

--- scripts/navHandlers.js
+++ scripts/navHandlers.js
@@ -14,15 +14,15 @@ const removeActivities = () => {
 const updateActiveButtons = (path) => {
   removeActivities();
   switch (path) {
-    case "":
-      document.querySelector('a[href*="#"]').classList.add("active-button");
+    case "/":
+      document.querySelector('a[href*="/"]').classList.add("active-button");
       break;
-    case "#map":
+    case "/map":
       ymaps.ready(init);
-      document.querySelector('a[href*="#map"]').classList.add("active-button");
+      document.querySelector('a[href*="/map"]').classList.add("active-button");
       break;
-    case "#time":
-      document.querySelector('a[href*="#time"]').classList.add("active-button");
+    case "/time":
+      document.querySelector('a[href*="/time"]').classList.add("active-button");
       break;
   }
 };
--- scripts/router.js
+++ scripts/router.js
@@ -8,23 +8,22 @@ const route = (event) => {
 };
 
 const routes = {
-  "": "/Web-bee-test-assignment/pages/activity.html",
-  "#map": "/Web-bee-test-assignment/pages/map.html",
-  "#time": "/Web-bee-test-assignment/pages/time.html",
+  "/": "/Web-bee-test-assignment/pages/activity.html",
+  "/map": "/Web-bee-test-assignment/pages/map.html",
+  "/time": "/Web-bee-test-assignment/pages/time.html",
 };
 
 const handleLocation = async () => {
-  const path = window.location.hash;
+  const path = window.location.pathname;
   console.log(`path: ${path}`);
   const html = await fetch(routes[path]).then((data) => data.text());
-  console.log(`html: ${path}`);
   document.getElementById("app").innerHTML = html;
 
   updateActiveButtons(path);
 };
 
 // Отслеживает back, go
-window.onpopstate = handleLocation;
+window.addEventListener("popstate", handleLocation);
 
 // Для начальной обработки
 document.addEventListener("DOMContentLoaded", () => {
