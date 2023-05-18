d05ffb0f54d1de1156908565c6596258ff5462c1
REIGEN <daniil-yak@yandex.ru>
2023-05-18 09:28:17 +0300

    fix: отписка от setInterval на других страницах

--- scripts/time.js
+++ scripts/time.js
@@ -5,12 +5,13 @@ function calculateTime() {
     let timer = document.getElementById("time");
     if (timer) {
       timer.innerHTML = getTime();
-
       function getTime() {
         let currentTime = Date.parse(new Date());
         let liveTime = currentTime - startTime;
         return new Date(liveTime).toUTCString().substring(17, 25);
       }
+    } else {
+      clearInterval(checker);
     }
   }, 1000);
 }
